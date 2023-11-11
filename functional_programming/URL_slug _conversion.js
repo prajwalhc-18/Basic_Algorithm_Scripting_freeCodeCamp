6b2f1f37cb15069bfcd31f4037d7f44452832c45
Prajwal H C <68982347+prajwalhc-18@users.noreply.github.com>
2023-11-11 16:03:49 +0530

    Add files via upload

---
+++ functional_programming/URL_slug _conversion.js
@@ -0,0 +1,8 @@
+function urlSlug(title) {
+  return title.toLowerCase()
+             .trim()
+             .split(/\s+/)
+             .join("-")
+}
+
+console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"))
