b070673914f9d3593ed87274d46642b748ce0904
Prajwal H C <68982347+prajwalhc-18@users.noreply.github.com>
2023-12-11 10:36:47 +0530

    Add files via upload

---
+++ functional_programming/alphabet_position.js
@@ -0,0 +1,10 @@
+function alphabetPosition(text) {
+  return text
+    .toLowerCase()
+    .split("")
+    .filter((char) => /[a-z]/.test(char))
+    .map((char) => char.charCodeAt(0) - 96)
+    .join(" ");
+}
+
+console.log(alphabetPosition("prajwal"))
